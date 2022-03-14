20c3138a6495b0d6462f09c71d2143ab8fd410f1
bdlvhn <bdlvhn@outlook.com>
2022-03-14 21:12:43 +0900

    Spring Security Basic complete

---
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.cos.security1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cos.security1.model.User;
+
+// CRUD 함수를 JpaRepository가 들고 있음.
+// @Repository라는 어노테이션이 없어도 IoC 된다. JpaRepository를 상속했기 때문에
+public interface UserRepository extends JpaRepository<User, Integer> {
+	// findBy 규칙 -> Username 문법
+	public User findByUsername(String username);
+	public User findByEmail(String email);
+}
